3bb7e247ea7e823679d3819b043231ff3b1aac23
ArturYes <153022144+ArturYes@users.noreply.github.com>
2024-03-11 21:18:39 +0600

    Update create_tables.sql
    Copied the sql code to create tables

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,31 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE orders
+(
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    employee_id INT,
+    order_date DATE,
+    ship_city VARCHAR(255),
+    author INT
+);
+
+CREATE TABLE customers
+(
+    customer_id INT PRIMARY KEY,
+    company_name VARCHAR(255),
+    contact_name VARCHAR(255),
+    author INT,
+    FOREIGN KEY (author) REFERENCES orders (order_id)
+);
+
+CREATE TABLE employees
+(
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    title VARCHAR(255),
+    birth_date DATE,
+    notes VARCHAR(255),
+    author INT,
+    FOREIGN KEY (author) REFERENCES orders (order_id)
+);
