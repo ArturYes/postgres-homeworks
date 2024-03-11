e2c438a4217593e1bb02d2a1f1d57facf6ff656b
ArturYes <153022144+ArturYes@users.noreply.github.com>
2024-03-11 21:44:23 +0600

    Update create_tables.sql
    fix code

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,31 +1,26 @@
 -- SQL-команды для создания таблиц
-CREATE TABLE orders
-(
-    order_id INT PRIMARY KEY,
-    customer_id INT,
-    employee_id INT,
-    order_date DATE,
-    ship_city VARCHAR(255),
-    author INT
-);
-
 CREATE TABLE customers
 (
     customer_id INT PRIMARY KEY,
-    company_name VARCHAR(255),
-    contact_name VARCHAR(255),
-    author INT,
-    FOREIGN KEY (author) REFERENCES orders (order_id)
+    company_name VARCHAR(100),
+    contact_name VARCHAR(100)
 );
 
 CREATE TABLE employees
 (
     employee_id INT PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    title VARCHAR(255),
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    title VARCHAR(100),
     birth_date DATE,
-    notes VARCHAR(255),
-    author INT,
-    FOREIGN KEY (author) REFERENCES orders (order_id)
+    notes VARCHAR(100)
+);
+
+CREATE TABLE orders
+(
+    order_id INT PRIMARY KEY,
+    customer_id INT REFERENCES customers(customer_id),
+    employee_id INT REFERENCES employees(employee_id),
+    order_date DATE,
+    ship_city VARCHAR(100)
 );
